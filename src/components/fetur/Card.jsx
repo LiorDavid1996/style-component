9335abbdf6cec2408f08721e794bd01b5582a8a6
LiorDavid1996 <liordavid356@gmail.com>
2022-09-15 23:13:45 +0300

    extra on the card

--- src/components/fetur/Card.jsx
+++ src/components/fetur/Card.jsx
@@ -1,7 +1,7 @@
 import { CardStyle } from "../styles/Card.style"
 export default function Card({item:{id,title,body,image}}) {
     return(
-        <CardStyle>
+        <CardStyle layout={id%2===0 && 'row-reverse'}>
              <div>
         <h2>{title}</h2>
         <p>{body}</p>
